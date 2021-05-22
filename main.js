51f5bf5acf5a9e2cc8bad5ea715a80770499a88c
alimsadetov <alimsadetov2019@mail.ru>
2021-05-22 20:28:04 +0300

    on review by ilyas

---
+++ main.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+    let karta = document.getElementById("karta");
+    let width = window.innerWidth;
+    if (window.innerWidth >=1110){
+        width = 1110;
+    }
+
+    else if (window.innerWidth >=800){
+        width = 800;
+    }
+
+    else if (window.innerWidth >=688){
+        width = 688;
+    }
+
+    else if (window.innerWidth < 688){
+        width = window.innerWidth;
+    }
+
+    karta.src=`https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A868440aafb3dee28d51d49af268be5a2214c509065e74fc96f7d42c497c5358c&amp;width=${width}&amp;height=499&amp;lang=ru_RU&amp;scroll=true`;
+};
