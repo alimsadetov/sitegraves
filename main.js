dcdcd8fc046bdb0982dade9a47e48efea2c02d3f
alimsadetov <alimsadetov2019@mail.ru>
2021-05-27 17:47:37 +0300

    responsive

--- main.js
+++ main.js
@@ -13,8 +13,16 @@ window.onload=function(){
         width = 688;
     }
 
-    else if (window.innerWidth < 688){
-        width = window.innerWidth;
+    else if (window.innerWidth >= 500){
+        width = 500;
+    }
+
+    else if (window.innerWidth >= 400){
+        width = 400;
+    }
+
+    else if (window.innerWidth >= 320){
+        width = 320;
     }
 
     karta.src=`https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A868440aafb3dee28d51d49af268be5a2214c509065e74fc96f7d42c497c5358c&amp;width=${width}&amp;height=499&amp;lang=ru_RU&amp;scroll=true`;
